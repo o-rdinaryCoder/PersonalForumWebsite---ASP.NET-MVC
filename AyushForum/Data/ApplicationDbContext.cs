c0542a018b4f896e53d54b9045ebce6c6195726b
o-rdinaryCoder <sangaiyasha@gmail.com>
2024-11-21 21:48:00 -0800

    First

---
+++ AyushForum/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Forum.Models;
+
+namespace Forum.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Answer> Answers { get; set; }
+    }
+}
---
+++ AyushForum/Data/Migrations/20230601131413_Second mgr.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Forum.Data.Migrations
+{
+    public partial class Secondmgr : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Answers_AspNetUsers_IdentityUserId",
+                table: "Answers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Questions_AspNetUsers_IdentityUserId",
+                table: "Questions");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IdentityUserId",
+                table: "Questions",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IdentityUserId",
+                table: "Answers",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Answers_AspNetUsers_IdentityUserId",
+                table: "Answers",
+                column: "IdentityUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Questions_AspNetUsers_IdentityUserId",
+                table: "Questions",
+                column: "IdentityUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Answers_AspNetUsers_IdentityUserId",
+                table: "Answers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Questions_AspNetUsers_IdentityUserId",
+                table: "Questions");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IdentityUserId",
+                table: "Questions",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IdentityUserId",
+                table: "Answers",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Answers_AspNetUsers_IdentityUserId",
+                table: "Answers",
+                column: "IdentityUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Questions_AspNetUsers_IdentityUserId",
+                table: "Questions",
+                column: "IdentityUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+    }
+}
